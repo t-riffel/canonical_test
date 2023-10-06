946ba2230a479932ed88916718694538a0febaf7
Todd Riffel <todd.e.riffel@gmail.com>
2023-10-05 23:26:11 -0500

    Getting the file size and opening it

--- shred.go
+++ shred.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	// make sure we're using shred correctly
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: go run shred.go <file-path>")
 		os.Exit(1)
@@ -15,9 +21,22 @@ func main() {
 }
 
 func Shred(filePath string) error {
+	// get some info on the file and check that it exists
 	fileInfo, err := os.Stat(filePath)
 	if os.IsNotExist(err) {
 		return fmt.Errorf("file not found: %s", filePath)
 	} else if err != nil {
 		return fmt.Errorf("error getting file info: %v", err)
 	}
+
+	fileSize := fileInfo.Size()
+
+	// open the file in read-write mode and handle errors
+	file, err := os.OpenFile(filePath, os.O_RDWR, 0666)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close() // Ensure file is closed after operation
+
+	return nil
+}
